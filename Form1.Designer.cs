d86a0a7dadfd01f06192b0098ac611061a9c6c2b
kaganarsln <kaan.arslangorurr@gmail.com>
2024-11-10 19:51:17 +0300

    CSHARP_REKURSIF

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace overloading_o1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Sayı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2. Sayı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(329, 170);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 40);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(329, 243);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 36);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Asal(Overloading)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(357, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Asal(Rekürsif)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(554, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 292);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 584);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace overloading_o1
+{
+    public partial class Form1 : Form
+    {
+        private int sayac;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void asal(int sayi)
+        {
+            int sayac = 0;
+            for (int i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    sayac++;
+                    break;
+                }
+            }
+
+            if (sayac == 0)
+            {
+                listBox1.Items.Add("asaldır" + sayi);
+            }
+            else
+            {
+                listBox1.Items.Add("asal sayı değildir." + sayi);
+            }
+        }
+
+        public void asal(int baslangic, int son)
+        {
+            for (int i = baslangic; i <= son; i++)
+            {
+                int sayac = 0;
+                for (int j = 2; j < baslangic; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sayac++;
+                        break;
+                    }
+
+                    if (sayac == 0)
+                    {
+                        listBox1.Items.Add("asaldır." + i);
+                    }
+
+                    else
+                    {
+                        listBox1.Items.Add("asal değildir." + i);
+                    }
+                }
+            }
+        }
+
+        public void asal(params int[] sayilar)
+        {
+            Random rastgeleSayi = new Random();
+            int uSayi = Convert.ToInt32(textBox1.Text);
+            int sayi = 0;
+            for (int i = 0; i < uSayi; i++)
+            {
+                sayi = rastgeleSayi.Next(1, 100);
+                int sayac = 0;
+                for (int j = 2; j < sayi; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sayac++;
+                    }
+
+                }
+
+                if (sayac == 0)
+                {
+                    listBox1.Items.Add("asaldır." + sayi);
+                }
+                else
+                {
+                    listBox1.Items.Add("asal değildir." + sayi);
+
+                }
+
+            }
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("1.Metot");
+                asal(sayi1);
+                listBox1.Items.Add("-----");
+                listBox1.Items.Add("3. Metot");
+                asal();
+            }
+            else
+            {
+                listBox1.Items.Clear();
+                int sayi2 = Convert.ToInt32(textBox2.Text);
+                listBox1.Items.Add("2. Metot");
+                asal(sayi1, sayi2);
+
+            }
+
+
+        }
+
+        public Boolean Asal(int sayi)
+        {
+            bool asal = true;
+            for (int i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0 && i != 1)
+                {
+                    asal = false;
+                }
+            }
+            return asal;
+        }
+
+        public void asalSayi(int baslangic, int bitis)
+        {
+            if (Asal(baslangic))
+            {
+                listBox1.Items.Add(baslangic.ToString() + "asaldır.");
+            }
+            baslangic++;
+            if (baslangic <= bitis)
+            {
+                asalSayi(baslangic, bitis);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            asalSayi(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+
+        }
+    }
+}
